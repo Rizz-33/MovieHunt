f09b9d5b1338ad6fcf6875965a967a3628054b3f
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-06-06 16:31:30 +0530

    feat: Get movie by id

--- src/main/java/rizz33/aarr/moviehunt/MovieService.java
+++ src/main/java/rizz33/aarr/moviehunt/MovieService.java
@@ -1,9 +1,11 @@
 package rizz33.aarr.moviehunt;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,5 +14,9 @@ public class MovieService {
 
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> oneMovie(ObjectId id){
+        return movieRepository.findById(id);
     }
 }
