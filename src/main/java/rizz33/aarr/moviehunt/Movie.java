bc8208ea9f4f3a1ba86cbee73243d5461c02b690
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-06-06 15:22:12 +0530

    refactor: Movie class

--- src/main/java/rizz33/aarr/moviehunt/Movie.java
+++ src/main/java/rizz33/aarr/moviehunt/Movie.java
@@ -1,12 +1,20 @@
 package rizz33.aarr.moviehunt;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Document(collation = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
+    @Id
     private ObjectId id;
     private String imdbId;
     private String title;
