b1e77d8dd98769153bc50f4f436e0bd9e923ceb5
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-06-06 14:12:37 +0530

    first commit

---
+++ src/main/java/rizz33/aarr/moviehunt/MoviehuntApplication.java
@@ -0,0 +1,13 @@
+package rizz33.aarr.moviehunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviehuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviehuntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rizz33/aarr/moviehunt/MoviehuntApplicationTests.java
@@ -0,0 +1,13 @@
+package rizz33.aarr.moviehunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviehuntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
